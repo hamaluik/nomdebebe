ddd2ae8dbad20a1131d9e07940e52210c3701b52
Kenton Hamaluik <kenton@hamaluik.ca>
2021-04-14 07:56:46 -0600

    added names list export

--- lib/screens/settings/data_export.dart
+++ lib/screens/settings/data_export.dart
@@ -2,19 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:namekit/blocs/settings/settings.dart';
+import 'package:namekit/blocs/names/names.dart';
 import 'package:namekit/keys.dart';
+import 'package:share/share.dart';
 
 class DataExport extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(
-        builder: (BuildContext context, SettingsState state) {
-      return ListTile(
-          key: Keys.settingsExport,
-          title: Text("Export"),
-          trailing: Icon(FontAwesomeIcons.fileExport),
-          leading: Icon(FontAwesomeIcons.table),
-          onTap: () async {});
+        builder: (BuildContext context, SettingsState settingsState) {
+      return BlocBuilder<NamesBloc, NamesState>(
+          builder: (BuildContext context, NamesState namesState) {
+        return ListTile(
+            key: Keys.settingsExport,
+            title: Text("Export"),
+            trailing: namesState.likedNames.isEmpty
+                ? null
+                : Icon(FontAwesomeIcons.chevronRight),
+            leading: Icon(FontAwesomeIcons.table),
+            subtitle: namesState.likedNames.isEmpty
+                ? Text("(not until you like some names first!)")
+                : null,
+            onTap: namesState.likedNames.isEmpty
+                ? null
+                : () async {
+                    String nameList =
+                        namesState.likedNames.map((n) => n.name).join(", ");
+                    try {
+                      await Share.share(nameList,
+                          subject: "My favourite bébé names");
+                    } catch (e) {
+                      print("error: " + e.toString());
+                      await showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Error"),
+                              content: SingleChildScrollView(
+                                  child: Text(
+                                      "Something went wrong when trying to export your list of favourite babynames, sorry!")),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('OK 🤷'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          });
+                    }
+                  });
+      });
     });
   }
 }
