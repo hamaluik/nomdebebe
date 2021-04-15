3180dbd8dcb76b91a9a43d65db34354738e80bf6
Kenton Hamaluik <kenton@hamaluik.ca>
2021-04-14 22:53:01 -0600

    added expanding options to name tiles

--- app/lib/screens/liked_screen.dart
+++ app/lib/screens/liked_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nomdebebe/blocs/names/names.dart';
 import 'package:nomdebebe/blocs/settings/settings.dart';
 import 'package:nomdebebe/widgets/name_tile.dart';
@@ -35,7 +36,35 @@ class _LikeScreenState extends State<LikedScreen> {
               child: ReorderableListView.builder(
                   itemBuilder: (BuildContext context, int index) => NameTile(
                       namesState.likedNames[index],
-                      key: Key(namesState.likedNames[index].id.toString())),
+                      key: Key(namesState.likedNames[index].id.toString()),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextButton.icon(
+                                onPressed: () =>
+                                    BlocProvider.of<NamesBloc>(context).add(
+                                        NamesDislike(
+                                            namesState.likedNames[index])),
+                                icon: Icon(FontAwesomeIcons.solidThumbsDown,
+                                    color: Colors.white),
+                                label: Text("Dislike",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .button
+                                        ?.copyWith(color: Colors.white))),
+                            TextButton.icon(
+                                onPressed: () =>
+                                    BlocProvider.of<NamesBloc>(context).add(
+                                        NamesUndecide(
+                                            namesState.likedNames[index])),
+                                icon: Icon(FontAwesomeIcons.question,
+                                    color: Colors.white),
+                                label: Text("Un-decide",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .button
+                                        ?.copyWith(color: Colors.white))),
+                          ])),
                   itemCount: namesState.likedNames.length,
                   onReorder: (int oldIndex, int newIndex) =>
                       BlocProvider.of<NamesBloc>(context)
--- app/lib/widgets/name_tile.dart
+++ app/lib/widgets/name_tile.dart
@@ -6,31 +6,61 @@ import 'package:nomdebebe/blocs/settings/settings.dart';
 import 'package:nomdebebe/models/sex.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NameTile extends StatelessWidget {
+class NameTile extends StatefulWidget {
   final Name name;
+  final Widget? child;
 
-  const NameTile(this.name, {Key? key}) : super(key: key);
+  const NameTile(this.name, {Key? key, this.child}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _NameTileState();
+}
+
+// TODO: make this animate opening and closing
+class _NameTileState extends State<NameTile> {
+  bool _isExpanded = false;
+
+  void _handleTap() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<NamesBloc, NamesState>(
         builder: (BuildContext context, NamesState state) {
       return BlocBuilder<SettingsBloc, SettingsState>(
-          builder: (BuildContext context, SettingsState settingsState) =>
-              Padding(
-                  padding: EdgeInsets.all(2),
-                  child: Card(
-                      color: sexToColour(
-                          context, name.sex, settingsState.pinkAndBlue),
-                      elevation: 2,
-                      child: Padding(
+          builder: (BuildContext context, SettingsState settingsState) {
+        return Padding(
+            padding: EdgeInsets.all(2),
+            child: Card(
+                color: sexToColour(
+                    context, widget.name.sex, settingsState.pinkAndBlue),
+                elevation: 2,
+                child: InkWell(
+                    onTap: widget.child == null ? null : _handleTap,
+                    child: Column(mainAxisSize: MainAxisSize.min, children: [
+                      Padding(
                           padding: EdgeInsets.all(8),
-                          child: Text(name.name,
+                          child: Text(widget.name.name,
                               style: Theme.of(context)
                                   .textTheme
                                   .headline4!
                                   .copyWith(color: Colors.white),
-                              textAlign: TextAlign.center)))));
+                              textAlign: TextAlign.center)),
+                      ClipRect(
+                          child: Align(
+                              alignment: Alignment.center,
+                              heightFactor: _isExpanded ? 1.0 : 0.0,
+                              child: Column(children: [
+                                Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 16),
+                                    child: Divider(height: 2, thickness: 2)),
+                                widget.child ?? Container()
+                              ]))),
+                    ]))));
+      });
     });
   }
 }
